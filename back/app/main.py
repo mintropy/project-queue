946326545bdadaa9b277c4eae3ff94a8a8abff43
mintropy <jun2korea@gmail.com>
2022-12-29 14:15:56 +0900

    back) FastAPI JWT

--- back/app/main.py
+++ back/app/main.py
@@ -1,10 +1,35 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+from pydantic import BaseModel
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
+
+SECRET_KEY = "0ce050565cb9535ed86717bf3b24d5400870fe86ea3b03995cd47e9b6e8add5b"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
 
 app = FastAPI()
 
